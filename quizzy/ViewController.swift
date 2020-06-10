16782d199b34bab3ba1749f2e8cbaf42716f0c18
vtadevosian <vahram_tadevosyan@edu.aua.am>
2020-06-10 12:20:02 +0400

    Initial Commit

---
+++ quizzy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  quizzy
+//
+//  Created by Vahram Tadevosian on 6/10/20.
+//  Copyright © 2020 Vahram Tadevosian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
